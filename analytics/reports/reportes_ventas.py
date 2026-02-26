1025cae0ad4b981ba5e863ed6c14b21ed0a3e584
FrankitoDIamon <francoo20014@gmail.com>
2026-02-25 23:49:53 -0300

    reportes_ventas.py

--- analytics/reports/reportes_ventas.py
+++ analytics/reports/reportes_ventas.py
@@ -1,16 +1,22 @@
-from analytics.utils.get_bdd import obtener_ventas
+from analytics.utils.get_bdd import obtener_boletas
 import pandas as pd
 
 def reporte_ventas_diarias():
-    df= obtener_ventas()
-    df ["fecha"] = pd.to_datetime(df["fecha"])
-    resumen = df.groupby(df["fecha"].dt.date).agg(
-        total_ventas = pd.NamedAgg(column="total", aggfunc="sum"),
-        cantidad_productos = pd.NamedAgg(column="producto_id", aggfunc="count")
+    df= obtener_boletas()
+
+    df["Fecha_Venta"] = df["Fecha_Venta"].astype(str).str.zfill(8)
+    df["Fecha_Venta"] = pd.to_datetime(df["Fecha_Venta"], format="%d%m%Y")
+
+    resumen= df.groupby(df["Fecha_Venta"].dt.date).agg(
+        total_ventas = ("Total_Venta", "sum"),
+        cantidad_boletas = ("ID_Venta", "count")
     ).reset_index()
-    resumen["ticket_promedio"] = resumen["total_ventas"] / resumen["cantidad_productos"]
+
+    resumen["ticket_promedio"] = (resumen["total_ventas"] / resumen["cantidad_boletas"])
+    resumen = resumen.sort_values("Fecha_Venta")
     print(resumen)
     resumen.to_excel("analytics/data/ventas_diarias.xlsx", index= False)
+    
 
 if __name__ == "__main__":
     reporte_ventas_diarias()
--- analytics/utils/get_bdd.py
+++ analytics/utils/get_bdd.py
@@ -1,12 +1,32 @@
 import pandas as pd
 import sqlite3
 
-def obtener_ventas(db_path="pos/database/BDD_MINIMARKET.db"):
+def obtener_boletas(db_path="pos/database/BDD_MINIMARKET.db"):
     conn = sqlite3.connect(db_path)
-    df = pd.read_sql_query("SELECT * FROM Ventas", conn)
+
+    query = """
+    SELECT ID_Venta, Fecha_Venta, Total_Venta
+    FROM Ventas
+    """
+
+    df = pd.read_sql_query(query, conn)
     conn.close()
     return df
 
+def obtener_detalle_ventas(db_path="pos/database/BDD_MINIMARKET.db"):
+    conn = sqlite3.connect(db_path)
+
+    query = """
+    SELECT v.ID_Venta, v.Fecha_Venta,
+           d.ID_producto, d.Nombre_Producto,
+           d.Cantidad_Vendida, d.Valor_Unitario
+    FROM Ventas v
+    JOIN Ventas_Detalle d ON v.ID_Venta = d.ID_Venta
+    """
+
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
 
 def obtener_inventario(db_path="pos/database/BDD_MINIMARKET.db"):
     conn = sqlite3.connect(db_path)
